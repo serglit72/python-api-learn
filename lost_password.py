617162b66fc83f167e63689effc965550836d91e
Serglit <serglit72@gmail.com>
2021-12-12 22:45:24 -0800

    Assignment: Ex9  Lost Password update

--- lost_password.py
+++ lost_password.py
@@ -14,14 +14,14 @@ for each in passwords:
 url1 = "https://playground.learnqa.ru/ajax/api/get_secret_password_homework"
 url2 = "https://playground.learnqa.ru/ajax/api/check_auth_cookie"
 login = "super_admin"
-i=0
+i = 0
 
-for each in unique:
-    response1 = requests.post(url1, data={'login': login, 'password': each})
+for pswd in unique:
+    response1 = requests.post(url1, data={'login': login, 'password': pswd})
     i += 1
     cookie_auth = dict(response1.cookies)
-    print(f"Attempt: {i}", cookie_auth, f" with password:{each} you're NOT authorized")
     check_auth_cookie = requests.get(url2, cookies=cookie_auth)
     if "NOT" not in check_auth_cookie.text:
-        print(check_auth_cookie.text, f" with password: '{each}' ")
+        print(f"Attempt: {i}", cookie_auth, f" with password:{each}", check_auth_cookie.text)
         break
+    print(f"Attempt: {i}", cookie_auth, f" with password:{each}", check_auth_cookie.text)
