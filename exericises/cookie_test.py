7afff9b6353640e1379fdd617cf7762e74907f15
Serglit <serglit72@gmail.com>
2021-12-16 17:12:47 -0800

    Created BaseCase class and modified test_user_auth.py. Cleaned up new framework

---
+++ exericises/cookie_test.py
@@ -0,0 +1,14 @@
+import requests
+import pytest
+
+
+def test_cookie_print():
+
+    url = "https://playground.learnqa.ru/api/homework_cookie"
+
+    response = requests.get(url)
+    cookie = dict(response.cookies)
+
+    print(f"The value for given cookie is {cookie['HomeWork']}")
+
+    assert "HomeWork" in cookie, "No 'HomeWork' in cookies"
