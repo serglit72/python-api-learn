2c697d5a7acf4a78b6f42440c2e44a39c7671fa7
Serglit <serglit72@gmail.com>
2021-12-25 22:34:23 -0800

    Ex17: negative tests for PUT method

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -2,6 +2,7 @@ from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
 
+
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
 
@@ -16,6 +17,7 @@ class TestUserEdit(BaseCase):
         first_name = register_data['firstName']
         password = register_data['password']
         user_id = self.get_json_value(response1, "id")
+
 
         # LOGIN
         login_data = {
@@ -54,3 +56,85 @@ class TestUserEdit(BaseCase):
             "Wrong name of the user after edit "
         )
 
+        # EDIT if unauthorized (Ex17: 1)
+
+        new_name = "Changed Name "
+
+        response5 = MyRequests.put(
+            f"/user/{user_id}",
+            # headers={"x-csrf-token": token},
+            # cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response5, 400), "It should have status_code=400," \
+                                                       " but responded with others"
+
+        # EDIT if authorized but for different user (EX17: 2)
+
+        new_name = "Changed again Name"
+        unauthorised_user_id = 2
+        response6 = MyRequests.put(
+            f"/user/{unauthorised_user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response6, 200)
+
+        try:
+            Assertions.assert_json_value_by_name(
+                response4,
+                "firstName",
+                new_name,
+                "Wrong name of the user after edit "
+            )
+
+        except: Exception(
+             print(f"You're not allowed to change value {new_name} for the existing user ")
+        )
+
+
+        # EDIT user email consisted an error (Ex17: 3)
+
+        email_err1 = email.replace('@',"_")
+        print(email_err1)
+        response7 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": email_err1}
+        )
+        try:
+            Assertions.assert_code_status(response7, 400)
+            Assertions.assert_json_value_by_name(
+                response7,
+                "email",
+                email_err1,
+                "Wrong format of the email after edit "
+            )
+        except: Exception(
+             print(f"Wrong format for email input")
+        )
+        # EDIT user email consisted only one char (Ex17: 4)
+
+        email_err2 = email[:1]
+
+        response8 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": email_err2}
+        )
+        try:
+            Assertions.assert_code_status(response8, 400)
+            Assertions.assert_json_value_by_name(
+                response8,
+                "email",
+                email_err2,
+                "Wrong email format after edit "
+            )
+        except: Exception(
+             print(f"Wrong format for email input")
+        )
